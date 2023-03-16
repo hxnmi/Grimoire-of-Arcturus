92c2fdd4370a0097cf939382067a3a0aeac9ff00
Satriyo Aji Nugroho <acttogreen@gmail.com>
2023-03-16 19:56:44 +0700

    Little Fix

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 public class LookAtCamera : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        Vector3 rotationVector = transform.rotation.eulerAngles;
-        rotationVector.y = Camera.main.transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.Euler(rotationVector);
+        transform.LookAt(Camera.main.transform);
     }
 }
