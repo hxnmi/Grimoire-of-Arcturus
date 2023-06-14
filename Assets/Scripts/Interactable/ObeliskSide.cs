6ea524824f08fa47afb1e7cc4fe03bd53986e658
Hanif <85260485+hxnmi@users.noreply.github.com>
2023-06-14 11:18:40 +0700

    add interact obelisk

---
+++ Assets/Scripts/Interactable/ObeliskSide.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObeliskSide : Interactable
+{
+	void InteractObelisk()
+	{
+		// set active minigame
+		
+	}
+
+	public override string GetDescription()
+	{
+		return "Press [F] to interact Obelisk .";
+	}
+
+	public override void Interact()
+	{
+		InteractObelisk();
+	}
+}
